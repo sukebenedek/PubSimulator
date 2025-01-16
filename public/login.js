7321f7ef3715c31b91ec0a6bab5a9d99a4416e42
“Zalán <tofalvi.zalan@students.jedlik.eu>
2025-01-16 18:51:56 +0100

    #8 html és egy kis js

---
+++ public/login.js
@@ -0,0 +1,27 @@
+"use strict";
+const chooseDIV = document.getElementById("choose");
+const loginDIV = document.getElementById("login");
+const registerDIV = document.getElementById("register");
+const LoginButton = document.getElementById("loginbtn");
+const RegisterButton = document.getElementById("registerbtn");
+const BackButton = document.getElementsByClassName("backbtn");
+function showLogin() {
+    chooseDIV === null || chooseDIV === void 0 ? void 0 : chooseDIV.classList.add("d-none");
+    registerDIV === null || registerDIV === void 0 ? void 0 : registerDIV.classList.add("d-none");
+    loginDIV.classList.remove("d-none");
+}
+function showRegister() {
+    chooseDIV === null || chooseDIV === void 0 ? void 0 : chooseDIV.classList.add("d-none");
+    loginDIV.classList.add("d-none");
+    registerDIV.classList.remove("d-none");
+}
+function backToChoose() {
+    loginDIV.classList.add("d-none");
+    registerDIV.classList.add("d-none");
+    chooseDIV === null || chooseDIV === void 0 ? void 0 : chooseDIV.classList.remove("d-none");
+}
+LoginButton === null || LoginButton === void 0 ? void 0 : LoginButton.addEventListener("click", showLogin);
+RegisterButton === null || RegisterButton === void 0 ? void 0 : RegisterButton.addEventListener("click", showRegister);
+Array.from(BackButton).forEach(element => {
+    element.addEventListener("click", backToChoose);
+});
---
+++ src/login.ts
@@ -0,0 +1,31 @@
+const chooseDIV = document.getElementById("choose");
+const loginDIV = document.getElementById("login");
+const registerDIV = document.getElementById("register");
+
+const LoginButton = document.getElementById("loginbtn"); 
+const RegisterButton = document.getElementById("registerbtn");
+const BackButton = document.getElementsByClassName("backbtn");
+
+function showLogin() {
+    chooseDIV?.classList.add("d-none");
+    registerDIV?.classList.add("d-none");
+    loginDIV!.classList.remove("d-none");
+}
+
+function showRegister() {
+    chooseDIV?.classList.add("d-none");
+    loginDIV!.classList.add("d-none");
+    registerDIV!.classList.remove("d-none");
+}
+
+function backToChoose() {
+    loginDIV!.classList.add("d-none");
+    registerDIV!.classList.add("d-none");
+    chooseDIV?.classList.remove("d-none");
+}
+
+LoginButton?.addEventListener("click", showLogin);
+RegisterButton?.addEventListener("click", showRegister);
+Array.from(BackButton).forEach(element => {
+    element.addEventListener("click", backToChoose);
+});
