726036a0d32ed82982f5530b9f8bb7c41312c71d
Roncz <roncz.oliver@students.jedlik.eu>
2025-01-16 15:11:47 +0100

    sorba jönnek az emberek veletlenszeruen mar benne vannak a divbe #3

--- public/orders.js
+++ public/orders.js
@@ -1,12 +1,19 @@
 import { fetchData } from './interfaces.js';
 const allGuests = await fetchData("http://localhost:3000/guests");
-console.log(allGuests[1]);
+const allDrinks = await fetchData("http://localhost:3000/drinks");
 let queue = [];
 function incomingOrder() {
+    let orders = document.getElementById("orders");
+    let random = Math.floor(Math.random() * allGuests.length);
     if (queue.length < 10) {
-        let randomGuest = allGuests[Math.floor(Math.random() * allGuests.length)];
+        let randomGuest = allGuests[random];
         queue.push(randomGuest);
+        orders.innerHTML +=
+            `<div class="order">
+            <img class="customerImg" src="${randomGuest.img}"/>
+            <p class="customerName">${randomGuest.name}</p>
+        </div>`;
         console.log(queue);
     }
 }
-setInterval(incomingOrder, 10000);
+setInterval(incomingOrder, 1000);
--- src/orders.ts
+++ src/orders.ts
@@ -1,20 +1,25 @@
 import {User, Ingredient, Drink, Guest, Order, load, fetchData} from './interfaces.js';
 
 const allGuests: Guest[] = await fetchData<Guest[]>("http://localhost:3000/guests");
-
-console.log(allGuests[1]);
+const allDrinks: Drink[] = await fetchData<Drink[]>("http://localhost:3000/drinks");
 
 let queue: Guest[] = [];
 
 function incomingOrder() {
+let orders = document.getElementById("orders");
+let random = Math.floor(Math.random() * allGuests.length);
     if (queue.length < 10) {
-        let randomGuest = allGuests[Math.floor(Math.random() * allGuests.length)];
+        let randomGuest = allGuests[random];
         queue.push(randomGuest);
+        orders!.innerHTML += 
+        `<div class="order">
+            <img class="customerImg" src="${randomGuest.img}"/>
+            <p class="customerName">${randomGuest.name}</p>
+        </div>`;
         console.log(queue);
     }
 }
 
-
-setInterval(incomingOrder, 10000); 
+setInterval(incomingOrder, 1000); 
 
 
