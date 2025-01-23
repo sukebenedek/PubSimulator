b58a40ab183b9800e03c885f968ac1e7083a580a
zalan0815 <tofalvi.zalan@students.jedlik.eu>
2025-01-23 11:22:30 +0100

    #11 log

--- public/guest.js
+++ public/guest.js
@@ -1,7 +1,11 @@
 "use strict";
-var _a;
+var _a, _b;
 function closePopup() {
     document.getElementById("popup").classList.add("d-none");
     document.getElementById("container").classList.remove("d-none");
 }
 (_a = document.getElementById("okButton")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", closePopup);
+function order() {
+    console.log("alkohol");
+}
+(_b = document.getElementById("counter")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", order);
