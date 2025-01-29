041e0b6eb227a4792e9422dc14892a4504f4b7f8
zalan0815 <tofalvi.zalan@students.jedlik.eu>
2025-01-29 14:18:06 +0100

    #11 itallap

--- public/guest.js
+++ public/guest.js
@@ -4,8 +4,13 @@ function closePopup() {
     document.getElementById("popup").classList.add("d-none");
     document.getElementById("container").classList.remove("d-none");
 }
+function closePopup2() {
+    document.getElementById("popup2").classList.add("d-none");
+}
 (_a = document.getElementById("okButton")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", closePopup);
 function order() {
-    console.log("alkohol");
+    var _a;
+    document.getElementById("popup2").classList.remove("d-none");
+    (_a = document.getElementById("order")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", closePopup2);
 }
 (_b = document.getElementById("counter")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", order);
--- src/guest.ts
+++ src/guest.ts
@@ -2,13 +2,16 @@ function closePopup() {
     document.getElementById("popup")!.classList.add("d-none");
     document.getElementById("container")!.classList.remove("d-none");
 }
+function closePopup2() {
+    document.getElementById("popup2")!.classList.add("d-none");
+}
 
 document.getElementById("okButton")?.addEventListener("click", closePopup);
 
 
 function order() {
-    console.log("alkohol");
-    
+    document.getElementById("popup2")!.classList.remove("d-none");   
+    document.getElementById("order")?.addEventListener("click", closePopup2); 
 }
 
 document.getElementById("counter")?.addEventListener("click", order);
