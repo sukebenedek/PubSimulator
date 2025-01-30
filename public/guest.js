cc0e6b7c93f1a334989acd25ee3e4fba876b09b0
zalan0815 <tofalvi.zalan@students.jedlik.eu>
2025-01-30 11:18:30 +0100

    #11 már valami működik

--- public/guest.js
+++ public/guest.js
@@ -1,5 +1,6 @@
 var _a, _b;
 import { fetchData } from './functions.js';
+let loaded = false;
 function closePopup() {
     document.getElementById("popup").classList.add("d-none");
     document.getElementById("container").classList.remove("d-none");
@@ -12,9 +13,24 @@ async function order() {
     var _a;
     document.getElementById("popup2").classList.remove("d-none");
     (_a = document.getElementById("cancel")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", closePopup2);
-    const drinks = await fetchData("http://localhost:3000/drinks");
-    drinks.forEach(d => {
-        //document.getElementById(d.category)
-    });
+    if (!loaded) {
+        const drinks = await fetchData("http://localhost:3000/drinks");
+        drinks.forEach(d => {
+            var _a;
+            let li = document.createElement('li');
+            li.style.listStyleType = 'none';
+            li.innerHTML = `
+                <div class="drinkItem">
+                            <div class="shadow">
+                                <img class="drinkImg" src="${d.img}" alt="${d.name}">
+                            </div>
+                            <span class="drinkText">${d.name} - ${d.price}Ft</span>
+                        </div>
+            `;
+            (_a = document.getElementById(d.category)) === null || _a === void 0 ? void 0 : _a.appendChild(li);
+        });
+        loaded = true;
+    }
+    ;
 }
 (_b = document.getElementById("counter")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", order);
--- src/guest.ts
+++ src/guest.ts
@@ -1,6 +1,7 @@
 import { Drink } from './interfaces.js';
 import { fetchData } from './functions.js';
 
+let loaded = false;
 
 function closePopup() {
     document.getElementById("popup")!.classList.add("d-none");
@@ -14,13 +15,26 @@ document.getElementById("okButton")?.addEventListener("click", closePopup);
 
 
 async function order() {
-    document.getElementById("popup2")!.classList.remove("d-none");   
-    document.getElementById("cancel")?.addEventListener("click", closePopup2); 
+    document.getElementById("popup2")!.classList.remove("d-none");
+    document.getElementById("cancel")?.addEventListener("click", closePopup2);
 
-    const drinks: Drink[] = await fetchData("http://localhost:3000/drinks");
-    drinks.forEach(d => {
-        //document.getElementById(d.category)
-    });
+    if(!loaded) {
+        const drinks: Drink[] = await fetchData("http://localhost:3000/drinks");    
+        drinks.forEach(d => {
+            let li = document.createElement('li');
+            li.style.listStyleType = 'none';
+            li.innerHTML = `
+                <div class="drinkItem">
+                            <div class="shadow">
+                                <img class="drinkImg" src="${d.img}" alt="${d.name}">
+                            </div>
+                            <span class="drinkText">${d.name} - ${d.price}Ft</span>
+                        </div>
+            `;
+            document.getElementById(d.category)?.appendChild(li);
+        });
+        loaded = true;
+    };
 }
 
 document.getElementById("counter")?.addEventListener("click", order);
