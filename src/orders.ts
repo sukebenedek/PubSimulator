82eabdee8df62320c3898b76888e9243edd39791
sukebenedek <suke.benedek@students.jedlik.eu>
2025-02-13 10:51:24 +0100

    #6 rendel

--- src/orders.ts
+++ src/orders.ts
@@ -1,20 +1,46 @@
 import { User, Ingredient, Drink, Guest, } from './interfaces.js';
-import { fetchData, randomNum, } from './functions.js';
-import { emptyGlass } from './ingredients.js';
-
+import { drawImage, drawRect, fetchData, randomN, randomNum } from "./functions.js";
 
 export let glass: Drink ;
 
-export function loadGlass(){
-    glass = queue[0].order[0]
+export function loadGlass(index: number = 0){
+    if(index === 0){
+        glass = queue[0].order[0]
+    }
+    else{
+        glass = queue[0].order[index]
+    }
 
 }
 const allGuests: Guest[] = await fetchData<Guest[]>("http://localhost:3000/guests");
 const allDrinks: Drink[] = await fetchData<Drink[]>("http://localhost:3000/drinks");
 
 let queue: Guest[] = [];
-console.log(1);
 
+
+let ingredients = await fetchData<Ingredient[]>("http://localhost:3000/ingredients")
+let c = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = c.getContext("2d") as CanvasRenderingContext2D
+c.height = 775
+c.width = 950
+let height = c.height;
+let width = c.width;
+
+// incomingOrder()
+
+drawImage("https://raw.githubusercontent.com/sukebenedek/PubSimulator/refs/heads/main/img/ingredients/cup3.png", 0, 0, width, height, ctx)
+let drinkType:Ingredient = ingredients[0];
+
+let currentDrink = 0
+let interval: ReturnType<typeof setInterval>;
+const glassConstant = 0.1;
+const glassBottom = 50
+const rowHeight = 14
+const glassStart = 230
+const cup = new Image();
+cup.src = "https://raw.githubusercontent.com/sukebenedek/PubSimulator/refs/heads/main/img/ingredients/cup3.png"
+let liquidHeight = 0
+let div =    document.getElementById("drinks") as HTMLDivElement
 
 export function incomingOrder() {
     let orders = document.getElementById("orders");
@@ -48,11 +74,11 @@ export function incomingOrder() {
         // console.log(queue);
     }
     receiveOrder();
-    loadGlass();
+    // loadGlass(); nem tudom mit csinal !!!!
 }
 
 function randomIncomingOrder() {
-    receiveOrder();
+    incomingOrder();
     const randomDelay = randomNum(10000);
     setTimeout(() => {
         incomingOrder();
@@ -68,6 +94,11 @@ export function receiveOrder() { //kiírja a rendelést és frissíti az ital me
     if (queue.length == 0) {
         sum!.innerHTML = "Nincs rendelés!";
     } else {
+        if(glass == undefined){
+            loadGlass()
+        }
+
+
         let orderListHTML = `
             <h1 class="text-center" style="margin-top:10px">Rendelés összesítő</h1>
             <h3 id="currentOrder" class="text-center mb-4">${queue[0].name}</h3>
@@ -93,7 +124,7 @@ export function receiveOrder() { //kiírja a rendelést és frissíti az ital me
             for (let j = 0; j < drink.ingredientsRequired.length; j++) {
                 const ingredient = drink.ingredientsRequired[j];
 
-                console.log(glass);
+                // console.log(glass);
                 
                 const ingredientInCup = glass.ingredientsInCup.find(i => i.name == ingredient.name);
                 const ingredientAmout = ingredientInCup ? ingredientInCup.amount * 10 : 0;
@@ -157,8 +188,10 @@ export function receiveOrder() { //kiírja a rendelést és frissíti az ital me
                 const drink = queue[0].order[i];
                 let drinkClick = document.getElementById(drink.name) as HTMLDivElement
                 drinkClick?.addEventListener("click", ()=>{
-                    console.log(drink.ingredientsRequired);
-                    console.log(glass.ingredientsInCup);
+                    // console.log(drink.ingredientsRequired);
+                    // console.log(glass.ingredientsInCup);
+
+                    loadGlass(i)
                     
                 } )
 
@@ -166,9 +199,8 @@ export function receiveOrder() { //kiírja a rendelést és frissíti az ital me
         }
 
     }
+
 }
-
-
 
 function getCustomerData() {
     let sum = document.getElementById('sum');
@@ -228,6 +260,104 @@ function declineOrder() {
 
 randomIncomingOrder();
 receiveOrder();
+
+
+
+
+
+
+
+c?.addEventListener("mousedown", (e) => {
+    ctx.fillStyle = drinkType.color;
+    currentDrink = 0
+    let r = randomN(50, 100)
+    let pre = liquidHeight
+    interval = setInterval(function() {
+            if(!(liquidHeight >= height - glassBottom )){
+            currentDrink++
+            r = randomN(50, 100)
+            // console.log(currentDrink);
+            liquidHeight = pre + currentDrink * rowHeight
+            // glass.ingredientsInCup.forEach((ingredient) => {
+            //     liquidHeight += ingredient.amount
+            // })
+
+            // console.log(liquidHeight);
+            
+            
+
+            drawRect(glassStart - liquidHeight * glassConstant, height - glassBottom - liquidHeight , width - glassStart - glassStart + liquidHeight * glassConstant * 2, rowHeight, ctx)
+            ctx.drawImage(cup, 0, 0, width, height);
+
+            // drawRect(10, 100, 100, 100, ctx)
+
+            
+            
+        } else{
+            // console.log("tele van"); 
+        }
+        }, r);        
+})
+
+c?.addEventListener("mouseup", (e) => {
+    clearInterval(interval);
+    drinkType.amount += currentDrink
+    if (!glass.ingredientsInCup.some(ingredient => ingredient.name === drinkType.name)) {
+        glass.ingredientsInCup.push(drinkType);
+    }
+    
+    drawGlass(glass);
+    console.log(glass);
+    receiveOrder();
+})
+
+
+ingredients.forEach(i => {
+    // console.log(i);
+    i.amount= 0
+    div.innerHTML += `<div class="ingredientCard card m-1 ${i.name} asd" id=""  style="width: 140px;">
+    <img src="${i.img}" class="card-img-top my-2 ingredient" alt="...">
+    <div class="card-body m-0">
+    <p class="m-0">${i.name}</p>
+    </div>
+    </div>`
+});
+ingredients.forEach(i => {
+    let a = document.querySelector(`.${i.name}`) as HTMLDivElement
+    a?.addEventListener("click", () => {selectIngredient(i)})
+    
+})
+
+function selectIngredient(i: Ingredient){
+    const allDrinkDiv = document.getElementsByClassName("selected");
+    Array.from(allDrinkDiv).forEach(div => {
+        div.classList.remove("selected");
+    });
+    drinkType = i;
+    const drinkDiv = document.querySelector(`.${i.name}`) as HTMLDivElement;
+    drinkDiv.classList.add("selected") 
+}
+
+function drawGlass(g: Drink){
+    // console.log(height);
+    
+}
+
+export function emptyGlass(g: Drink) {
+    ctx.clearRect(0, 0, width, height); 
+    drawImage("https://raw.githubusercontent.com/sukebenedek/PubSimulator/refs/heads/main/img/ingredients/cup3.png", 0, 0, width, height, ctx); 
+
+    g.ingredientsInCup = []; 
+    liquidHeight = 0; 
+    currentDrink = 0; 
+
+    ingredients.forEach(i => i.amount = 0);
+
+    ctx.fillStyle = drinkType.color;
+    drawRect(glassStart - liquidHeight * glassConstant, height - glassBottom - liquidHeight , width - glassStart - glassStart + liquidHeight * glassConstant * 2, rowHeight, ctx)
+    ctx.drawImage(cup, 0, 0, width, height);
+}
+
 
 
 export {queue}
