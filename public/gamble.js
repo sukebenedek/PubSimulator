efd4732c23d9f0d9e00e3ad2de4b07e6007cf4df
sukebenedek <suke.benedek@students.jedlik.eu>
2025-02-20 10:35:51 +0100

    #5 gamble

--- public/gamble.js
+++ public/gamble.js
@@ -15,7 +15,7 @@ function closeGamblePopup() {
 const player = document.getElementById("player");
 const dealer = document.getElementById("dealer");
 const cardImages = [
-    "2_of_clubs.png", "2_of_diamonds.png", "2_of_hearts.png", "2_of_spades.png",
+    "2_of_clubs.png", "2_of_diamonds.png", "_of_hearts.png", "2_of_spades.png",
     "3_of_clubs.png", "3_of_diamonds.png", "3_of_hearts.png", "3_of_spades.png",
     "4_of_clubs.png", "4_of_diamonds.png", "4_of_hearts.png", "4_of_spades.png",
     "5_of_clubs.png", "5_of_diamonds.png", "5_of_hearts.png", "5_of_spades.png",
