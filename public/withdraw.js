5a1ef5f2271c0e00bc0bffebaec2f7fdd79079ca
zalan0815 <tofalvi.zalan@students.jedlik.eu>
2025-03-20 10:28:36 +0100

    #4 pénzfeltöltés

--- public/withdraw.js
+++ public/withdraw.js
@@ -1,6 +1,37 @@
-import { showUser } from './user.js';
+import { isNumber } from './functions.js';
+import { showUser, setMoney } from './user.js';
 if (localStorage.getItem("user") == null || localStorage.getItem("user") == undefined || localStorage.getItem("user") == "") {
     window.location.replace("./login.html");
 }
 let user = JSON.parse(localStorage.getItem("user"));
 showUser(document.body, user);
+document.getElementById("withdrawButton").addEventListener("click", withdraw);
+document.getElementById("cancel").addEventListener("click", () => {
+    window.location.replace("./index.html");
+});
+function withdraw() {
+    if (checkInputs()) {
+        setMoney(Number(document.getElementById("amountInput").value));
+        window.location.replace("./index.html");
+    }
+}
+function checkInputs() {
+    let good = true;
+    let cardNumber = document.getElementById("carNumberInput").value;
+    let amount = document.getElementById("amountInput").value;
+    if (!isNumber(cardNumber)) {
+        document.getElementById("carNumberInput").classList.add("bg-danger");
+        good = false;
+    }
+    else {
+        document.getElementById("carNumberInput").classList.remove("bg-danger");
+    }
+    if (!isNumber(amount)) {
+        document.getElementById("amountInput").classList.add("bg-danger");
+        good = false;
+    }
+    else {
+        document.getElementById("amountInput").classList.remove("bg-danger");
+    }
+    return good;
+}
--- src/withdraw.ts
+++ src/withdraw.ts
@@ -1,5 +1,5 @@
 import { User } from './interfaces.js';
-import { postData, patchData } from './functions.js';
+import { postData, patchData, isNumber } from './functions.js';
 import { showUser, getUser, setMoney } from './user.js';
 
 if (localStorage.getItem("user") == null || localStorage.getItem("user") == undefined || localStorage.getItem("user") == "" ) {
@@ -8,3 +8,39 @@ if (localStorage.getItem("user") == null || localStorage.getItem("user") == unde
 
 let user: User = JSON.parse(localStorage.getItem("user")!);
 showUser(document.body, user);
+
+document.getElementById("withdrawButton")!.addEventListener("click", withdraw);
+document.getElementById("cancel")!.addEventListener("click", () => {
+    window.location.replace("./index.html");
+});
+
+function withdraw() {
+    if (checkInputs()) {
+        setMoney(Number((<HTMLInputElement>document.getElementById("amountInput")!).value));
+        window.location.replace("./index.html");
+    }
+}
+
+function checkInputs() {
+    let good = true;
+    let cardNumber = (<HTMLInputElement>document.getElementById("carNumberInput")!).value;
+    let amount = (<HTMLInputElement>document.getElementById("amountInput")!).value;
+
+    if (!isNumber(cardNumber)) {
+        document.getElementById("carNumberInput")!.classList.add("bg-danger");
+        good = false;
+    }
+    else {
+        document.getElementById("carNumberInput")!.classList.remove("bg-danger");
+    }
+
+    if (!isNumber(amount)) {
+        document.getElementById("amountInput")!.classList.add("bg-danger");
+        good = false;
+    }
+    else {
+        document.getElementById("amountInput")!.classList.remove("bg-danger");
+    }
+
+    return good;
+}
